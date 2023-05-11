878e5dad0527ee430ff428571001f3a594af3c00
James Partin <jamesaustinpartin@gmail.com>
2023-05-10 20:10:15 -0400

    .

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ToasterSwerveBase";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "02917e06fc2213fe4ab231cf472fed7187bcd793";
-  public static final String GIT_DATE = "2023-05-09 02:21:38 EDT";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "a930c23508310d73dbcf305d111fe4788e76c886";
+  public static final String GIT_DATE = "2023-05-10 20:04:09 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-05-10 19:49:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1683762556677L;
+  public static final String BUILD_DATE = "2023-05-10 20:07:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1683763653930L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,15 +3,14 @@ package frc.robot;
 public final class Constants {
     public static final Mode currentMode = Mode.REAL;
     public static boolean DeterministicTimestamps = true;
-
     public static enum Mode {
         /** Running on a real robot. */
         REAL,
-
         /** Running a physics simulator. */
         SIM,
-
         /** Replaying from a log file. */
         REPLAY
     }
+
+
 }
