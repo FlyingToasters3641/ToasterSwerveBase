460f2f739dcd0aead969067c7e54efd4ce844b47
James Partin <jamesaustinpartin@gmail.com>
2023-05-12 01:35:29 -0400

    added value store class

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ToasterSwerveBase";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "a930c23508310d73dbcf305d111fe4788e76c886";
-  public static final String GIT_DATE = "2023-05-10 20:04:09 EDT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "7f06322055cba0f147fdbae724f1ebff47358eb2";
+  public static final String GIT_DATE = "2023-05-11 18:52:06 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-05-10 20:07:33 EDT";
-  public static final long BUILD_UNIX_TIME = 1683763653930L;
+  public static final String BUILD_DATE = "2023-05-11 19:09:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1683846587550L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
